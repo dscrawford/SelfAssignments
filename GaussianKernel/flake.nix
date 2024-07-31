ae3bec7917e751a94a1ddec435b11e6b523aa6b3
Daniel Crawford <daniel.sc.crawford@gmail.com>
2024-07-31 15:03:03 -0700

    Update flake to work with Nix run

--- GaussianKernel/flake.nix
+++ GaussianKernel/flake.nix
@@ -9,18 +9,47 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem(system :
       let
-        system = "x86_64-linux";
         pkgs = import nixpkgs { system = system; };
+        pythonPackages = ps: with ps; with pkgs; [
+          pandas
+          numpy
+          matplotlib
+          scikit-learn
+          pip
+        ];
+        pythonEnv = pkgs.python3.withPackages pythonPackages;
+        # Define a package that includes `main.py`
+        myPackage = pkgs.stdenv.mkDerivation {
+          pname = "gaussian-kernel-test";
+          version = "1.0";
+          
+          src = ./.; # Source directory
+
+          buildInputs = [ pythonEnv pkgs.bash ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ${myPackage.src}/main.py $out/bin/main.py
+            chmod +x $out/bin/main.py
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Gaussian Kernel test";
+            license = licenses.mit;
+          };
+        };
       in {
+        packages.default = myPackage;
+
+        defaultPackage = myPackage;
+        
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            python3
-            python3Packages.pandas
-            python3Packages.numpy
-            python3Packages.matplotlib
-            python3Packages.scikit-learn
-            python3Packages.pip
-          ];
+          buildInputs = [ pythonEnv ];
+        };
+        
+        defaultApp = {
+          type = "app";
+          program = "${pythonEnv}/bin/python ${myPackage}/bin/main.py";
         };
       });
 }
